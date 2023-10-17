b9d5963eac6bb2d237c4f0851fe7601edf1d11ed
Kieran <kieran@annarbortees.com>
2023-10-17 18:40:20 -0400

    removing huge amount of pseudocode from index file

--- index.php
+++ index.php
@@ -33,58 +33,6 @@ class chowChooserEngine {
 	
 	function __construct() {
 		
-		/*
-		
-		Pseudo code:
-		
-		1. Ways a user can access ChowChooser
-			A. From visiting the base url
-			B. From getting an invitiation link to an existing order
-			
-		2. Which page is a user greeted with for these methods:
-			A. Welcome page, prompted to select a new order or join an order by entering a key
-			B. View order page, directly joining an existing order using URL key
-			
-		3. What keys do we need to check for in the URL?
-			A. Welcome page shows if no get params are detected
-			B. View Order page as long as order_key exists 
-			
-		
-		How does this make sense from key detection logic?
-		
-		1. We need to look for an orderKey key existing in the URL
-			
-			- If we find it, we can try to view the order
-				-- This results in view_order page being called
-				
-			- If we have the orderKey but not value is assigned, show welcome page but with warning stating that user must supply key
-			- If we have the orderKey but assigned value was not found in database, show welcome page but with warning staing that order with key was not found. 
-				-- These two result in returning to the welcome page, but with an error displayed
-				
-			- If we have an orderKey and also an action, we can try to interact with the Order
-				- Run command for action before returning to view order page
-				- Available commands are:
-					- startNewOrder: begins a new order by generating a key and redirecting to view_order page
-					- addFoodOption: adds a food option to the current order
-					- deleteFoodOption: removes a food option from the current order
-					- updateFoodOption: alter an existing food option from current order
-					- voteFoodOption: place a vote for this food option
-					- unvoteFoodOption: remove a vote fo rthis food option
-						-- These all result in some php processing on the backend and then return user to view page
-						
-		This to glean from notes:
-			- The only action that can be performed without a orderKey is startNewOrder, which shouldn't be called anywhere except the base welcome page
-			- Other actions cannot be performed without an orderKey
-			 
-		so we can assume:
-			- User with no keys at all goes to welcome page (if no orderKey or actionKey exists)
-			- User with an order key and no other action goes to view (if no actionKey exists but orderKey does) yes to 
-			- User with an order key and actions goes to process, then view (if orderKey Exists and actionKey exists) yes to both
-			- Other case: if an user provides an action with no order key (no orderKey, actionKey != startNewOrder) send them to welcome page and say no key specified
-			
-	
-			
-		*/
 		
 		$orderKeyExists = key_exists("orderKey", $_GET);
 		$actionKeyExists = key_exists("action", $_GET);
