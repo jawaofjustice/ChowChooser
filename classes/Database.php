c4f759eb3a0d7bd49334e387ddab83ccc7bf7ad2
massivebird <gdrakemail@gmail.com>
2023-11-07 16:54:38 -0500

    style: changes indent style to tabs

--- classes/Database.php
+++ classes/Database.php
@@ -2,61 +2,61 @@
 
 class Database {
 
-   private mysqli $mysqli;
+	private mysqli $mysqli;
 
 	function __construct() {
 		$pass = getenv('CHOWCHOOSER_P');
 		$this->mysqli = new mysqli("localhost","chowChooserAdmin",$pass,"chow_chooser");
 
 		if ($this->mysqli->connect_errno) {
-		  echo "Failed to connect to MySQL: " . $this->mysqli->connect_error;
-		  exit();
+			echo "Failed to connect to MySQL: " . $this->mysqli->connect_error;
+			exit();
 		}
 		return $this;
 	}
 
-   public function getUserFromCredentials(string $email, string $password): User|null {
-      // when creating an account, we're only concerned with finding
-      // accounts with the inputted email (emails should be unique)
-      if (is_null($password)) {
-         $statement = $this->mysqli->prepare("select * from user where email = (?) limit 1");
-         $statement->bind_param('s', $email);
-      } else {
-         $statement = $this->mysqli->prepare("select * from user where email = (?) and password = (?) limit 1");
-         $statement->bind_param('ss', $email, $password);
-      }
+	public function getUserFromCredentials(string $email, string $password): User|null {
+		// when creating an account, we're only concerned with finding
+		// accounts with the inputted email (emails should be unique)
+		if (is_null($password)) {
+			$statement = $this->mysqli->prepare("select * from user where email = (?) limit 1");
+			$statement->bind_param('s', $email);
+		} else {
+			$statement = $this->mysqli->prepare("select * from user where email = (?) and password = (?) limit 1");
+			$statement->bind_param('ss', $email, $password);
+		}
 
-      $statement->execute();
-      $user = mysqli_fetch_assoc($statement->get_result());
+		$statement->execute();
+		$user = mysqli_fetch_assoc($statement->get_result());
 
-      // no such user exists with these credentials
-      if (is_null($user)) {
-         return null;
-      }
+		// no such user exists with these credentials
+		if (is_null($user)) {
+			return null;
+		}
 
-      return new User($user['id'], $user['email']);
-   }
+		return new User($user['id'], $user['email']);
+	}
 
-   public function getUsersLobbies(int $id) {
-      /* $statement = $this->mysqli->prepare("select * from lobby as l inner join lobby_user as lu on l.id = lu.lobby_id where lu.user_id = (?)"); */
-      $statement = $this->mysqli->prepare("select * from lobby where lobby.admin_id = (?)");
-      $statement->bind_param('s', $id);
-      $statement->execute();
-      /* $lobbies = mysqli_fetch_assoc($statement->get_result()); */
-      foreach ($statement->get_result() as $lobby) {
-         print_r($lobby);
-         echo "<br>";
-      }
-   }
+	public function getUsersLobbies(int $id) {
+		/* $statement = $this->mysqli->prepare("select * from lobby as l inner join lobby_user as lu on l.id = lu.lobby_id where lu.user_id = (?)"); */
+		$statement = $this->mysqli->prepare("select * from lobby where lobby.admin_id = (?)");
+		$statement->bind_param('s', $id);
+		$statement->execute();
+		/* $lobbies = mysqli_fetch_assoc($statement->get_result()); */
+		foreach ($statement->get_result() as $lobby) {
+			print_r($lobby);
+			echo "<br>";
+		}
+	}
 
 	function createAccount(string $email, string $password): void {
-      $id = $this->getUserFromCredentials($email, null);
-      if (!is_null($id)) {
-         echo "Cannot create account: user already exists with this email.";
-         return;
-      }
-      echo "Can create account: user does not exist with this email!";
-   }
+		$id = $this->getUserFromCredentials($email, null);
+		if (!is_null($id)) {
+			echo "Cannot create account: user already exists with this email.";
+			return;
+		}
+		echo "Can create account: user does not exist with this email!";
+	}
 
 }
 
