6137c29a05931ee69845d82e5db6ae828dc8ead6
Kieran <kieran@annarbortees.com>
2023-12-09 21:41:17 -0500

    adding some indentiation

--- classes/ChowChooserEngine.php
+++ classes/ChowChooserEngine.php
@@ -230,9 +230,9 @@ class ChowChooserEngine {
 		$this->swapArray['userName'] = $_SESSION['user']->getUsername();
 		$this->swapArray['title'] = "Main Menu";
 		if (key_exists('errorMsg', $this->swapArray))
-		$swapArray['errorMsg'] = $this->swapArray['errorMsg'];
+			$swapArray['errorMsg'] = $this->swapArray['errorMsg'];
 		else
-		$swapArray['errorMsg'] = "";
+			$swapArray['errorMsg'] = "";
 
 
 		$all_user_lobbies = Lobby::getUsersLobbies($_SESSION['user']->getId());
